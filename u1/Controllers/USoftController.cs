bbcb06d7874ef2160176b2a4be43b236f00010ea
mac12_win <pacs3131@gmail.com>
2022-11-11 03:54:19 +0300

    - code cleaning

--- u1/Controllers/USoftController.cs
+++ u1/Controllers/USoftController.cs
@@ -7,57 +7,13 @@ using u1.DbData;
 using u1.Models;
 using X.PagedList;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace u1.Controllers
 {
     public class USoftController : Controller
     {
         //-------------------------------------------------
-        //List<OneRecord> recList;
         AbstractContext ac = new AbstractContext();
-        //public USoftController()
-        //{
-        //    recList = new List<OneRecord>();
-        //    for(int i = 0; i < 100000; ++i)
-        //    {
-        //        recList.Add(
-        //            new OneRecord(
-        //                i,
-        //                "text" + i,
-        //                (1 == (i & 1))
-        //        ));
-        //    }
-        //}
-        //-------------------------------------------------
-
-        //[HttpGet]
-        //public IActionResult USoft(int? page)
-        //{
-        //    var pageN = page ?? 1;
-        //    var pageSz = 10;
-
-        //    var onePage = recList.ToPagedList(pageN, pageSz);
-
-        //    USoftModel m = new USoftModel();
-        //    m.SelectedValue = pageSz;
-        //    return View(m);
-        //}
-
-        //[HttpGet]
-        //public IActionResult USoft(USoftModel m, int? page)
-        //{
-        //    var pageN = page ?? 1;
-        //    var pageSz = m.SelectedValue;
-
-        //    if (pageSz == 0) pageSz = 10;//
-
-        //    var onePage = recList.ToPagedList(pageN, pageSz);
-
-        //    m.pList = onePage;
-        //    return View(m);
-        //}
-
+        
         [HttpGet]
         public async Task<IActionResult> USoft(USoftModel m, int? page)
         {
--- u1/Models/EmptyClass.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace u1.Models
-{
-    public static class EmptyClass
-    {
-        public static List<int> list = new List<int> { 0, 1, 2, 3, 4, 5 };
-    }
-}
-
